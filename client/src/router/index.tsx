a05a7ca0bf2e2bc08fa11fc564a1a52f5d33a65c
akpatel-1 <akpatel.work1@gmail.com>
2026-04-13 15:25:13 +0530

    feat: add Prettier to auto sort inport on save

--- client/src/router/index.tsx
+++ client/src/router/index.tsx
@@ -1,8 +1,8 @@
-import { createBrowserRouter } from "react-router-dom";
+import { createBrowserRouter } from 'react-router-dom';
 
 export const router = createBrowserRouter([
   {
-    path: "/",
+    path: '/',
     element: <h1 className="bg-amber-200">Homepage</h1>,
   },
 ]);
