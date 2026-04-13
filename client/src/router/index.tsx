c63cea077c1960e9091e5af8e7ec2f8eb414cfc7
akpatel-1 <akpatel.work1@gmail.com>
2026-04-13 15:07:24 +0530

    feat: add Tailwind CSS library with tailwind.config.ts file

--- client/src/router/index.tsx
+++ client/src/router/index.tsx
@@ -3,6 +3,6 @@ import { createBrowserRouter } from "react-router-dom";
 export const router = createBrowserRouter([
   {
     path: "/",
-    element: <h1>Homepage</h1>,
+    element: <h1 className="bg-amber-200">Homepage</h1>,
   },
 ]);
