2e32f0d141fa2589f8a4cabb476a3524a8075b30
akpatel-1 <akpatel.work1@gmail.com>
2026-04-13 21:13:45 +0530

    feat: add cookie-parser and cors middleware for request handling

--- server/src/app.ts
+++ server/src/app.ts
@@ -1,3 +1,5 @@
+import cookieParser from 'cookie-parser';
+import cors from 'cors';
 import express from 'express';
 
 import { router } from './index.js';
@@ -5,8 +7,15 @@ import { globalErrorHandler } from './module/error-handler-middleware.js';
 
 export const app = express();
 
+app.use(
+  cors({
+    origin: process.env.FRONTEND_URL,
+    credentials: true,
+    methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'],
+  }),
+);
+
 app.use(express.json());
-
+app.use(cookieParser());
 app.use(router);
-
 app.use(globalErrorHandler);
--- server/src/module/admin/session/admin-session.config.ts
+++ server/src/module/admin/session/admin-session.config.ts
@@ -1,5 +1,4 @@
 export const ADMIN_SESSION_CONFIG = {
-    SESSION_PREFIX: 'admin:session:',
-    SESSION_TTL: 7 * 24 * 60 * 60,
-    
-}
+  SESSION_PREFIX: 'admin:session:',
+  SESSION_TTL: 7 * 24 * 60 * 60,
+};
