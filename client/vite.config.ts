e38dc3209f636c46b3f5125f980a2c63d8adcbcf
akpatel-1 <akpatel.work1@gmail.com>
2026-04-13 15:30:10 +0530

    refactor: update import paths and configure path aliases to '@'

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,8 +1,13 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
-import tailwindcss from "@tailwindcss/vite";
+import tailwindcss from '@tailwindcss/vite';
+import react from '@vitejs/plugin-react';
+import { URL, fileURLToPath } from 'node:url';
+import { defineConfig } from 'vite';
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  resolve: {
+    alias: {
+      '@': fileURLToPath(new URL('./src', import.meta.url)),
+    },
+  },
 });
