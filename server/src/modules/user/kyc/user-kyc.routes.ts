a48b04bd98627aa5ceff2fff38f97bcfd6b61fa7
akpatel-1 <akpatel.work1@gmail.com>
2026-04-16 09:22:55 +0530

    feat(kyc): add user KYC status retrieval functionality and update repository queries

--- server/src/modules/user/kyc/user-kyc.routes.ts
+++ server/src/modules/user/kyc/user-kyc.routes.ts
@@ -4,7 +4,7 @@ import { validateSchema } from '@/modules/schema-validator.js';
 
 import { upload } from '../file-upload.middleware.js';
 import { validateUserSession } from '../session/user-session.middleware.js';
-import { validateKycFile } from './kyc-middleware.js';
+import { validateKycFile } from './user-kyc-middleware.js';
 import { submitUserKyc, userKycStatus } from './user-kyc.controller.js';
 import { userKycSchema } from './user-kyc.schema.js';
 
